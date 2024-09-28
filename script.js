3be04f68528d40a0692ba66c2a322bf362ddbd46
TheHellLord645 <100252028+TheHellLord645@users.noreply.github.com>
2024-09-28 18:31:04 +0530

    Rewrote entire code 2 times just to add save feature

--- script.js
+++ script.js
@@ -1,65 +1,126 @@
-let todoTaskList = document.getElementById("todo-task-list");
-let doingTaskList = document.getElementById("doing-task-list");
-let doneTaskList = document.getElementById("done-task-list");
+let todoTaskListElement = document.getElementById("todo-task-list");
+let doingTaskListElement = document.getElementById("doing-task-list");
+let doneTaskListElement = document.getElementById("done-task-list");
 
-function AddTask() {
-    let task = window.prompt("Please enter a task:");
+// taskStatus = 0: "todo", 1: "doing", 2: "done"
+var tasks = [["Finish This Website."], [], []];
 
+function GetItemFormat(task) {
+    return `<div class="item">
+        <p>${task}</p>
+        <div class="buttons">
+            <button class="up-button" onclick="MoveTaskUp(this)">/\\</button>
+            <button class="close-button" onclick="RemoveTaskButton(this)">x</button>
+            <button class="down-button" onclick="MoveTaskDown(this)">\\/</button>
+            <button class="move-button" onclick="MoveTaskToNextStatus(this)">></button>
+        </div>
+    </div>`;
+}
+
+function AddTask(task, taskStatus) {
     if (task != "" && task != null) {
-        let item = `<div class="item">
-            <p>${task}</p>
-            <div class="buttons">
-                <button class="up-button" onclick="MoveTaskUp(this)">/\\</button>
-                <button class="close-button" onclick="RemoveTask(this)">x</button>
-                <button class="down-button" onclick="MoveTaskDown(this)">\\/</button>
-                <button class="move-button" onclick="MoveTaskToNextLayer(this)">></button>
-            </div>
-        </div>`;
-        todoTaskList.innerHTML += item;
+        tasks[taskStatus].push(task);
+        Update();
+        Save();
     }
 }
 
-function RemoveTask(button) {
-    let item = button.parentElement.parentElement;
-    item.remove();
+function AddTaskFromPrompt() {
+    let task = window.prompt("Please enter a task:");
+    AddTask(task, 0);
 }
 
-function MoveTaskToNextLayer(button) {
-    let item = button.parentElement.parentElement;
-    let id = item.parentElement.getAttribute('id');
+function AddTasksFromList(list, taskStatus) {
+    if (list == null) return;
+    for (let i = 0; i < list.length; i++) {
+        AddTask(list[i], taskStatus);
+    }
+}
 
-    if (id == "todo-task-list") {
-        doingTaskList.appendChild(item);
-    }
-    else if (id == "doing-task-list") {
-        doneTaskList.appendChild(item);
-    }
+function RemoveTask(task, taskStatus) {
+    let index = tasks[taskStatus].indexOf(task);
+    tasks[taskStatus].splice(index, 1);
+    Update();
+    Save();
+}
+
+function RemoveTaskButton(button) {
+    let parent = button.parentElement.parentElement;
+    let task = parent.children[0].innerHTML;
+    let taskStatus = parent.parentElement.getAttribute("data-status");
+    RemoveTask(task, taskStatus);
+}
+
+function SwapTask(index1, index2, taskStatus) {
+    let temp = tasks[taskStatus][index1];
+    tasks[taskStatus][index1] = tasks[taskStatus][index2];
+    tasks[taskStatus][index2] = temp;
+    Update();
 }
 
 function MoveTaskUp(button) {
     let item = button.parentElement.parentElement;
-    let parent = item.parentElement;
-    console.log(parent);
-    for (let index = 2; index < parent.children.length; index++) {
-        console.log(index);
-        if (parent.children[index] == item) {
-            let temp = parent.children[index-1].innerHTML;
-            parent.children[index-1].innerHTML = item.innerHTML;
-            parent.children[index].innerHTML = temp;
-        }
-    }
+    let task = item.children[0].innerHTML;
+    let status = item.parentElement.getAttribute("data-status");
+    let index = tasks[status].indexOf(task);
+    if (index == 0) return;
+    SwapTask(index, index-1, status);
 }
 
 function MoveTaskDown(button) {
     let item = button.parentElement.parentElement;
-    let parent = item.parentElement;
-    console.log(parent);
-    for (let index = 1; index < parent.children.length-1; index++) {
-        console.log(index);
-        if (parent.children[index] == item) {
-            let temp = parent.children[index+1].innerHTML;
-            parent.children[index+1].innerHTML = item.innerHTML;
-            parent.children[index].innerHTML = temp;
-        }
+    let task = item.children[0].innerHTML;
+    let status = item.parentElement.getAttribute("data-status");
+    let index = tasks[status].indexOf(task);
+    if (index+1 == tasks[status].length) return;
+    SwapTask(index, index+1, status);
+}
+
+function MoveTaskToNextStatus(button) {
+    let item = button.parentElement.parentElement;
+    let task = item.children[0].innerHTML;
+    let status = parseInt(item.parentElement.getAttribute("data-status"));
+
+    console.log(item);
+    console.log(task);
+    console.log(status);
+
+    if (status == 2) return;
+
+    AddTask(task, status+1);
+    RemoveTask(task, status);
+}
+
+function ReadLists() {
+    console.log(tasks);
+}
+
+function Save() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function Load() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+    Update();
+}
+
+function Update() {
+    todoTaskListElement.innerHTML = "";
+    doingTaskListElement.innerHTML = "";
+    doneTaskListElement.innerHTML = "";
+
+    for (let i = 0; i < tasks[0].length; i++) {
+        todoTaskListElement.innerHTML += GetItemFormat(tasks[0][i]);
+    }
+    for (let i = 0; i < tasks[1].length; i++) {
+        doingTaskListElement.innerHTML += GetItemFormat(tasks[1][i]);
+    }
+    for (let i = 0; i < tasks[2].length; i++) {
+        doneTaskListElement.innerHTML += GetItemFormat(tasks[2][i]);
     }
 }
+
+window.onload = Load();
+document.addEventListener("keypress", function(event) {
+    if (event.key == 'n') AddTaskFromPrompt();
+});
